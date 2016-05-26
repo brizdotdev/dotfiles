cddcc539397dce034a5b4b51d16aef5e546ab02f
Akshay Brizmohun <aksh1000@gmail.com>
2016-05-26 14:11:17 +1000

    more blocks to bar

--- scripts/IconicWeather.sh
+++ scripts/IconicWeather.sh
@@ -9,40 +9,47 @@ echo
 exit 0;
 fi
 
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
-{
-my $sun = "";
-binmode(STDOUT, ":utf8");
-print "$sun";
-}
-if( $values[0] eq "Mostly Cloudy" || $values[0] eq "Cloudy" || $values[0] eq "Dreary (Overcast)" || $values[0] eq "Fog")
-{
-my $cloud = "";
-binmode(STDOUT, ":utf8");
-print "$cloud";
-}
-if( $values[0] eq "Showers" || $values[0] eq "Mostly Cloudy w/ Showers" || $values[0] eq "Partly Sunny w/ Showers" || $values[0] eq "T-Storms"|| $values[0] eq "Mostly Cloudy w/ T-Storms"|| $values[0] eq "Partly Sunny w/ T-Storms"|| $values[0] eq "Rain")
-{
-my $rain = "";
-binmode(STDOUT, ":utf8");
-print "$rain";
-}
-if( $values[0] eq "Windy")
-{
-my $wind = "";
-binmode(STDOUT, ":utf8");
-print "$wind";
-}
-if($values[0] eq "Flurries" || $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Partly Sunny w/ Flurries"|| $values[0] eq "Snow"|| $values[0] eq "Mostly Cloudy w/ Snow"|| $values[0] eq "Ice"|| $values[0] eq "Sleet"|| $values[0] eq "Freezing Rain"|| $values[0] eq "Rain and Snow"|| $values[0] eq "Cold")
-{
-my $snow = "";
-binmode(STDOUT, ":utf8");
-print "$rain";
-}
-if($values[0] eq "Clear" || $values[0] eq "Mostly Clear" || $values[0] eq "Partly Cloudy"|| $values[0] eq "Intermittent Clouds"|| $values[0] eq "Hazy Moonlight"|| $values[0] eq "Mostly Cloudy"|| $values[0] eq "Partly Cloudy w/ Showers"|| $values[0] eq "Mostly Cloudy w/ Showers"|| $values[0] eq "Partly Cloudy w/ T-Storms"|| $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Mostly Cloudy w/ Snow")
-{
-my $night = "";
-binmode(STDOUT, ":utf8");
-print "$night";
-}
-print"$values[1]"; }'
+curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'use utf8;
+if (/Currently/) {
+  chomp;/\<title\>Currently: (.*)?\<\/title\>/;
+  my @values=split(":",$1);
+  my $q = $values[0];
+  $values[0]= lc($values[0]);
+  if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
+  {
+    my $sun = "";
+    binmode(STDOUT, ":utf8");
+    print "$sun";
+  }
+  if( $values[0] eq "mostly cloudy" || $values[0] eq "cloudy" || $values[0] eq "dreary (Overcast)" || $values[0] eq "fog")
+  {
+    my $cloud = "";
+    binmode(STDOUT, ":utf8");
+    print "$cloud";
+  }
+  if( $values[0] eq "showers" || $values[0] eq "mostly cloudy w/ showers" || $values[0] eq "mostly cloudy w/ showers" || $values[0] eq "partly sunny w/ showers" || $values[0] eq "t-storms"|| $values[0] eq "mostly cloudy w/ t-storms"|| $values[0] eq "partly sunny w/ t-storms"|| $values[0] eq "rain")
+  {
+    my $rain = "";
+    binmode(STDOUT, ":utf8");
+    print "$rain";
+  }
+  if( $values[0] eq "windy")
+  {
+    my $wind = "";
+    binmode(STDOUT, ":utf8");
+    print "$wind";
+  }
+  if($values[0] eq "flurries" || $values[0] eq "mostly cloudy w/ flurries" || $values[0] eq "partly sunny w/ flurries"|| $values[0] eq "snow"|| $values[0] eq "mostly cloudy w/ snow"|| $values[0] eq "ice"|| $values[0] eq "sleet"|| $values[0] eq "freezing rain"|| $values[0] eq "rain and snow"|| $values[0] eq "cold")
+  {
+    my $snow = "";
+    binmode(STDOUT, ":utf8");
+    print "$rain";
+  }
+  if($values[0] eq "clear" || $values[0] eq "mostly clear" || $values[0] eq "partly cloudy"|| $values[0] eq "intermittent clouds"|| $values[0] eq "hazy moonlight"|| $values[0] eq "mostly cloudy"|| $values[0] eq "partly cloudy w/ showers"|| $values[0] eq "mostly cloudy w/ showers"|| $values[0] eq "partly cloudy w/ t-storms"|| $values[0] eq "mostly cloudy w/ flurries" || $values[0] eq "mostly cloudy w/ snow")
+  {
+    my $night = "";
+    binmode(STDOUT, ":utf8");
+    print "$night";
+  }
+  print "$values[1] $q";
+}'
