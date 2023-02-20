99c545b063880d4442edf92514e46c75e3f57f76
Akshay Brizmohun <aksh1000@gmail.com>
2023-02-21 00:39:01 +1100

    Remove ansible based Win setup

--- win/scripts/get-win-ip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-## Get Windows IP address from WSL
-## https://learn.microsoft.com/en-us/windows/wsl/networking#accessing-windows-networking-apps-from-linux-host-ip
-cat /etc/resolv.conf | grep nameserver | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
--- win/scripts/setup-wsl-host-ip.ps1
+++
@@ -1,6 +0,0 @@
-# Setup the WINDOWS host IP address for WSL2 in the inventory file
-$wslIP = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object -Property InterfaceAlias -Like "*WSL*").IPAddress
-$inventory = $PSScriptRoot + "\..\ansible\hosts"
-$inventoryContent = Get-Content -Path $inventory -Raw
-$inventoryContent = $inventoryContent -Replace "{IP}", $wslIP
-$inventoryContent | Set-Content -Path $inventory
