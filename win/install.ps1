42df037329f2e19bf07b19d211813eefb7774989
Akshay Brizmohun <aksh1000@gmail.com>
2023-02-22 22:14:45 +1100

    ?

--- win/install.ps1
+++ win/install.ps1
@@ -101,5 +101,5 @@ Write-Host -ForegroundColor Green "SSH Key generated"
 Write-Host
 
 Write-Host -ForegroundColor Green "Done! 😄"
-Write-Host -ForegroundColor Green "You can run the following manually: $ENV:DOTFILES\win\scripts\Configure-WindowsTerminal.ps1 $ENV:DOTFILES\win\scripts\Install-WSL.ps1"
+Write-Host -ForegroundColor Green "You can run the following manually: $ENV:DOTFILES\win\scripts\Configure-WindowsTerminal.ps1 $ENV:DOTFILES\win\scripts\Install-WSL.ps1""
 Read-Host
