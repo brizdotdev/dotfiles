1688fce5247371ac680bec0ba962f7824ee98d29
Akshay Brizmohun <aksh1000@gmail.com>
2023-02-25 20:10:14 +1100

    Fix PowerShell aliases

--- win/config/PowerShell/profile.ps1
+++ win/config/PowerShell/profile.ps1
@@ -59,37 +59,37 @@ if (Test-Path $LocalProfile) {
 # WGet: Use `wget.exe` if available
 if (Get-Command wget.exe -ErrorAction SilentlyContinue | Test-Path) {
     rm alias:wget -ErrorAction SilentlyContinue
-  }
+}
 
-  # Directory Listing: Use `ls.exe` if available
-  if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
-      rm alias:ls -ErrorAction SilentlyContinue
-      # Set `ls` to call `ls.exe` and always use --color
-      ${function:ls} = { ls.exe --color @args }
-      # List all files in long format
-      ${function:l} = { ls -lF @args }
-      # List all files in long format, including hidden files
-      ${function:la} = { ls -laF @args }
-      # List only directories
-      ${function:lsd} = { Get-ChildItem -Directory -Force @args }
-  } else {
-      # List all files, including hidden files
-      ${function:la} = { ls -Force @args }
-      # List only directories
-      ${function:lsd} = { Get-ChildItem -Directory -Force @args }
-  }
+# Directory Listing: Use `ls.exe` if available
+if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:ls -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:ls} = { ls.exe --color @args }
+    # List all files in long format
+    ${function:l} = { ls -lF @args }
+    # List all files in long format, including hidden files
+    ${function:la} = { ls -laF @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+} else {
+    # List all files, including hidden files
+    ${function:la} = { ls -Force @args }
+    # List only directories
+    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+}
 
-  # curl: Use `curl.exe` if available
-  if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
-      rm alias:curl -ErrorAction SilentlyContinue
-      # Set `ls` to call `ls.exe` and always use --color
-      ${function:curl} = { curl.exe @args }
-      # Gzip-enabled `curl`
-      ${function:gurl} = { curl --compressed @args }
-  } else {
-      # Gzip-enabled `curl`
-      ${function:gurl} = { curl -TransferEncoding GZip }
-  }
+# curl: Use `curl.exe` if available
+if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
+    rm alias:curl -ErrorAction SilentlyContinue
+    # Set `ls` to call `ls.exe` and always use --color
+    ${function:curl} = { curl.exe @args }
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl --compressed @args }
+} else {
+    # Gzip-enabled `curl`
+    ${function:gurl} = { curl -TransferEncoding GZip }
+}
 del alias:sl -Force
 del alias:rm -Force
 del alias:sort -Force
@@ -113,6 +113,7 @@ Set-Alias -Name "dckr" -Value docker
 Set-Alias -Name "dn" -Value dotnet
 Set-Alias -Name "cat" -Value bat
 Set-Alias -Name "z" -Value zoxide
+Set-Alias -Name "mkdir" -Value mkdir.exe
 
 ################################################################################
 # Imports
