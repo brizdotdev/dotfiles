b4c02f45c47285d288ea0b8f4ad645edd18092fc
Akshay Brizmohun <aksh1000@gmail.com>
2023-03-25 13:05:12 +1100

    win: Remove Generate-SSHKey.ps1

--- win/scripts/Generate-SSHKey.ps1
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Generate an SSH Key
-################################################################################
-Write-Host -ForegroundColor Blue "Generating SSH Key"
-ssh-keygen -t ed25519
-Write-Host -ForegroundColor Green "SSH Key generated"
-Write-Host
