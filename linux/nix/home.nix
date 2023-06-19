cf0393daca881a3b57003acf01654efdf92a774f
Akshay Brizmohun <aksh1000@gmail.com>
2023-06-19 21:22:51 +1000

    Add devcontainer nix setup

---
+++ linux/nix/home.nix
@@ -0,0 +1,121 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "{{USERNAME}}";
+  home.homeDirectory = "{{HOME_PATH}}";
+
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # Dev utils
+    pkgs.bat
+    pkgs.git
+    pkgs.fzf
+    pkgs.delta
+    pkgs.exa
+    pkgs.less
+    pkgs.gnused
+    pkgs.gnugrep
+    # pkgs.neovim
+    pkgs.starship
+    pkgs.zoxide
+    pkgs.jq
+    pkgs.yq-go
+    pkgs.fd
+    pkgs.glab
+    pkgs.ripgrep
+    pkgs.lazygit
+    pkgs.lf
+    pkgs.glow
+    pkgs.xsv
+    pkgs.sd
+    pkgs.duf
+    pkgs.fx
+    pkgs.tmux
+
+    # Basic CLI Utils
+    pkgs.p7zip
+    pkgs.gnutar
+    pkgs.unzip
+    pkgs.zip
+
+    # Useful utils
+    pkgs.ffmpeg_6
+    pkgs.yt-dlp
+    pkgs.htop
+    pkgs.bottom
+    pkgs.dig
+    pkgs.gnupg
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+    ".bashrc".source = "${config.home.homeDirectory}/.dotfiles/linux/config/bash/bashrc";
+    ".config/lazygit/config.yml".source = "${config.home.homeDirectory}/.dotfiles/common/config/lazygit/config.yml";
+    ".config/nvim".source = "${config.home.homeDirectory}/.dotfiles/common/config/nvim";
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/vscode/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PAGER = "less";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+}
+
+# TODO: Setup config files (e.g Lazygit, starship, etc)
---
+++ linux/nix/install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+install_dependencies() {
+  if command -v curl &> /dev/null && command -v xz &> /dev/null
+  then
+    return
+  fi
+
+  if ! command -v sudo &> /dev/null
+  then
+      echo -e "\033[31m sudo is not installed \033[0m"
+      exit 1
+  fi
+
+  if command -v apt-get &> /dev/null
+  then
+      sudo apt-get update && sudo apt-get install -y curl xz
+      return
+  fi
+
+  if command -v yum &> /dev/null
+  then
+      sudo yum update && sudo yum install -y curl xz
+      return
+  fi
+
+  if command -v pacman &> /dev/null
+  then
+      sudo pacman -Sy curl xz
+      return
+  fi
+
+  if command -v apk &> /dev/null
+  then
+    sudo apk update && sudo apk add curl xz
+    return
+  fi
+}
+
+install_dependencies
+
+NIX_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
+
+# Install nix
+if [ ! -e "$NIX_SH" ]; then
+  echo -e "\033[32m Installing nix \033[0m"
+  sh <(curl -L https://nixos.org/nix/install) --no-daemon
+fi
+
+chmod +x "$NIX_SH"
+. "$NIX_SH"
+
+if ! command -v home-manager &> /dev/null; then
+  # Install home-manager
+  echo -e "\033[32m Installing home-manager \033[0m"
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+  nix-channel --update
+  nix-shell '<home-manager>' -A install
+fi
+
+# Install home-manager config
+HOME_MANAGER_TARGET="$HOME/.config/home-manager/home.nix"
+HOME_MANAGER_SOURCE="$HOME/.dotfiles/linux/nix/home.nix"
+echo -e "\033[32m Installing home-manager config \033[0m"
+rm "$HOME_MANAGER_TARGET"
+echo -e "\033[32m Replacing {{USERNAME}} with $USER \033[0m"
+sed -i "s+{{USERNAME}}+$USER+g" "$HOME_MANAGER_SOURCE"
+echo -e "\033[32m Replacing {{HOME_PATH}} with $HOME \033[0m"
+sed -i "s+{{HOME_PATH}}+$HOME+g" "$HOME_MANAGER_SOURCE"
+ln -s "$HOME_MANAGER_SOURCE" "$HOME_MANAGER_TARGET"
+home-manager switch -b bak
+
+source ~/.bashrc
