f5df01ca2e557a89b8e00622849f405b0eb5cb31
Akshay Brizmohun <aksh1000@gmail.com>
2024-04-11 21:28:41 +1000

    linux: Remove installing deps from nix/install.sh

--- linux/nix/install.sh
+++ linux/nix/install.sh
@@ -1,66 +1,20 @@
 #!/bin/bash
 
-install_dependencies() {
-  if command -v curl &> /dev/null && command -v xz &> /dev/null
-  then
-    return
-  fi
-
-  if ! command -v sudo &> /dev/null
-  then
-      echo -e "\033[31m sudo is not installed \033[0m"
-      exit 1
-  fi
-
-  if command -v apt-get &> /dev/null
-  then
-      sudo apt-get update && sudo apt-get install -y curl xz
-      return
-  fi
-
-  if command -v yum &> /dev/null
-  then
-      sudo yum update && sudo yum install -y curl xz
-      return
-  fi
-
-  if command -v pacman &> /dev/null
-  then
-      sudo pacman -Sy curl xz
-      return
-  fi
-
-  if command -v apk &> /dev/null
-  then
-    sudo apk update && sudo apk add curl xz
-    return
-  fi
-}
-
-install_dependencies
-
-NIX_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
-
 # Ensure submodules are initialized
 pushd $HOME/.dotfiles
 git submodule update --init --recursive
 popd
 
-# Install nix
-if [ ! -e "$NIX_SH" ]; then
-  echo -e "\033[32m Installing nix \033[0m"
-  sh <(curl -L https://nixos.org/nix/install) --no-daemon
+# Ensure nix is installed
+if ! command -v nix &> /dev/null; then
+  echo -e "\033[31m ⚠️ Nix is not installed \033[0m"
+  exit 1
 fi
 
-chmod +x "$NIX_SH"
-. "$NIX_SH"
-
-# Install home-manager
+# Ensure home-manager is installed
 if ! command -v home-manager &> /dev/null; then
-  echo -e "\033[32m Installing home-manager \033[0m"
-  nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
-  nix-channel --update
-  nix-shell '<home-manager>' -A install
+  echo -e "\033[31m ⚠️ Home-manager is not installed \033[0m"
+  exit 1
 fi
 
 # Install home-manager config
