17a6cddb6c86212173b3e62a8126ea7f8a69c480
Akshay Brizmohun <aksh1000@gmail.com>
2026-03-15 11:29:19 +1100

    win: Update bootstrap.ps1 to install git using winget config file

--- win/bootstrap.ps1
+++ win/bootstrap.ps1
@@ -24,70 +24,13 @@ function Update-WinGetFromPowerShellGallery([WinGetRelease]$Release = [WinGetRel
     } else {
         Repair-WinGetPackageManager -Latest
     }
-}
-
-function Add-ToPath(
-    [string[]]$Paths,
-    [System.EnvironmentVariableTarget]$Scope = [System.EnvironmentVariableTarget]::User
-) {
-    $currentPath = [System.Environment]::GetEnvironmentVariable("Path", $Scope)
-    $pathEntries = @()
-    if (-not [string]::IsNullOrWhiteSpace($currentPath)) {
-        $pathEntries = $currentPath -split ";"
-    }
-
-    foreach ($pathToAdd in $Paths) {
-        if ([string]::IsNullOrWhiteSpace($pathToAdd)) {
-            continue
-        }
-
-        if (-not ($pathEntries -contains $pathToAdd)) {
-            if ([string]::IsNullOrWhiteSpace($currentPath)) {
-                $currentPath = $pathToAdd
-            } else {
-                $currentPath = "$currentPath;$pathToAdd"
-            }
-            $pathEntries += $pathToAdd
-        }
-    }
-
-    [System.Environment]::SetEnvironmentVariable("Path", $currentPath, $Scope)
+    winget configure --enable
 }
 
 function Install-Git {
     $ErrorActionPreference = "Stop"
     Write-Host -ForegroundColor Blue "Installing Git"
-
-    $gitInstallPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Programs\Git"
-    # https://gitforwindows.org/silent-or-unattended-installation.html
-    # https://jrsoftware.org/ishelp/index.php?topic=setupcmdline
-    $gitInstallOverrides = @(
-        "/SP-"
-        "/VERYSILENT"
-        "/SUPPRESSMSGBOXES"
-        "/NORESTART"
-        "/NOCANCEL"
-        "/CURRENTUSER"
-        "/LOG"
-        "/DIR=`"$gitInstallPath`""
-        "/COMPONENTS=`"assoc,assoc_sh,gitlfs,scalar`""
-        "/TASKS=`"`""
-        "/o:SSHOption=ExternalOpenSSHPathOption=CmdTools"
-        "/o:CURLOption=WinSSL"
-        "/o:PathOption=CmdTools"
-        "/o:BashTerminalOption=ConHost"
-        "/o:EnableSymlinks=Enabled"
-        "/o:EnableFSMonitor=Enabled"
-        "/o:PerformanceTweaksFSCache=Enabled"
-    ) -join " "
-    winget install --scope user --silent --accept-source-agreements --accept-package-agreements --source winget --override $gitInstallOverrides Git.Git
-
-    $gitCmdPath = Join-Path -Path $gitInstallPath -ChildPath "cmd"
-    $gitBinPath = Join-Path -Path $gitInstallPath -ChildPath "bin"
-    $gitUsrBinPath = Join-Path -Path $gitInstallPath -ChildPath "usr\bin"
-    $gitPathsToAdd = @($gitCmdPath, $gitBinPath, $gitUsrBinPath)
-    Add-ToPath -Paths $gitPathsToAdd -Scope ([System.EnvironmentVariableTarget]::User)
-
+    winget configure --accept-configuration-agreements https://raw.githubusercontent.com/$GitHubUsername/$GitHubRepoName/refs/heads/main/win/scripts/config/git.winget
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
     Write-Host -ForegroundColor Green "Git installed"
 }
@@ -107,7 +50,7 @@ function Clone-Repo {
         Read-Host
         exit 1
     }
-    [Environment]::SetEnvironmentVariable("DOTFILES", $ParentPath, "User")
+    [Environment]::SetEnvironmentVariable("DOTFILES", $dotfilesPath, "User")
     Write-Host -ForegroundColor Green "Dotfiles cloned to $dotfilesPath"
     Write-Host -ForegroundColor Green "Run $dotfilesPath\win\install.ps1 to finish setup"
 }
