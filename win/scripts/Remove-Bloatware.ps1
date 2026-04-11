56421288895b247776ceab748a561381fa4d881a
Akshay Brizmohun <aksh1000@gmail.com>
2026-04-11 21:41:01 +1000

    win: Update Remove-Bloatware script

--- win/scripts/Remove-Bloatware.ps1
+++ win/scripts/Remove-Bloatware.ps1
@@ -1,188 +1,104 @@
 #Requires -RunAsAdministrator
 Write-Host -ForegroundColor Blue "Removing bloatware"
 
-Get-AppxPackage *3DBuilder* | Remove-AppxPackage ;
-Get-AppxPackage *ActiproSoftwareLLC* | Remove-AppxPackage ;
-Get-AppxPackage *AdobePhotoshopExpress* | Remove-AppxPackage ;
-Get-AppxPackage *AdobeSystemsIncorporated.AdobePhotoshopExpress* | Remove-AppxPackage ;
-Get-AppxPackage *Advertising.Xaml* | Remove-AppxPackage ;
-Get-AppxPackage *AutodeskSketchBook* | Remove-AppxPackage ;
-Get-AppxPackage *BethesdaSoftworks.FalloutShelter* | Remove-AppxPackage ;
-Get-AppxPackage *BingFinance* | Remove-AppxPackage ;
-Get-AppxPackage *BingNews* | Remove-AppxPackage ;
-Get-AppxPackage *BingSports* | Remove-AppxPackage ;
-Get-AppxPackage *BingWeather* | Remove-AppxPackage ;
-Get-AppxPackage *BubbleWitch* | Remove-AppxPackage ;
-Get-AppxPackage *BubbleWitch3Saga* | Remove-AppxPackage ;
-Get-AppxPackage *CandyCrush* | Remove-AppxPackage ;
-Get-AppxPackage *CommsPhone* | Remove-AppxPackage ;
-Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage ;
-Get-AppxPackage *Disney* | Remove-AppxPackage ;
-Get-AppxPackage *Dolby* | Remove-AppxPackage ;
-Get-AppxPackage *DolbyAccess* | Remove-AppxPackage ;
-Get-AppxPackage *Duolingo* | Remove-AppxPackage ;
-Get-AppxPackage *Duolingo-LearnLanguagesforFree* | Remove-AppxPackage ;
-Get-AppxPackage *Eclipse* | Remove-AppxPackage ;
-Get-AppxPackage *EclipseManager* | Remove-AppxPackage ;
-Get-AppxPackage *Facebook* | Remove-AppxPackage ;
-Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage ;
-Get-AppxPackage *Flipboard* | Remove-AppxPackage ;
-Get-AppxPackage *HiddenCity* | Remove-AppxPackage ;
-Get-AppxPackage *Keeper* | Remove-AppxPackage ;
-Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage ;
-Get-AppxPackage *Messaging* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.Asphalt8Airborne* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.BingFinance* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.DrawboardPDF* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.Messaging* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.Wallet* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage ;
-Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage ;
-Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage ;
-Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage ;
-Get-AppxPackage *Minecraft* | Remove-AppxPackage ;
-Get-AppxPackage *MinecraftUWP* | Remove-AppxPackage ;
-Get-AppxPackage *Office.OneNote* | Remove-AppxPackage ;
-Get-AppxPackage *Office.Sway* | Remove-AppxPackage ;
-Get-AppxPackage *OneConnect* | Remove-AppxPackage ;
-Get-AppxPackage *OneNote* | Remove-AppxPackage ;
-Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage ;
-Get-AppxPackage *Phone* | Remove-AppxPackage ;
-Get-AppxPackage *PhototasticCollage* | Remove-AppxPackage ;
-Get-AppxPackage *PowerBI* | Remove-AppxPackage ;
-Get-AppxPackage *Royal Revolt* | Remove-AppxPackage ;
-Get-AppxPackage *RoyalRev* | Remove-AppxPackage ;
-Get-AppxPackage *SketchBook* | Remove-AppxPackage ;
-Get-AppxPackage *Skype* | Remove-AppxPackage ;
-Get-AppxPackage *Sling* | Remove-AppxPackage ;
-Get-AppxPackage *Speed Test* | Remove-AppxPackage ;
-Get-AppxPackage *Sway* | Remove-AppxPackage ;
-Get-AppxPackage *Sway* | Remove-AppxPackage ;
-Get-AppxPackage *Todos* | Remove-AppxPackage ;
-Get-AppxPackage *Twitter* | Remove-AppxPackage ;
-Get-AppxPackage *Twitter* | Remove-AppxPackage ;
-Get-AppxPackage *Wallet* | Remove-AppxPackage ;
-Get-AppxPackage *WindowsAlarms* | Remove-AppxPackage ;
-Get-AppxPackage *WindowsMaps* | Remove-AppxPackage ;
-Get-AppxPackage *WindowsPhone* | Remove-AppxPackage ;
-Get-AppxPackage *Wunderlist* | Remove-AppxPackage ;
-Get-AppxPackage *XboxOneSmartGlass* | Remove-AppxPackage ;
-Get-AppxPackage *Zune* | Remove-AppxPackage ;
-Get-AppxPackage *ZuneMusic* | Remove-AppxPackage ;
-Get-AppxPackage *ZuneVideo* | Remove-AppxPackage ;
-Get-AppxPackage *bingsports* | Remove-AppxPackage ;
-Get-AppxPackage *flaregamesGmbH.RoyalRevolt2* | Remove-AppxPackage ;
-Get-AppxPackage *king.com.CandyCrushSodaSaga* | Remove-AppxPackage ;
-Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage ;
-Get-AppxPackage *twitter* | Remove-AppxPackage ;
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage ;
-Get-AppxPackage *windowsphone* | Remove-AppxPackage ;
-Get-AppxPackage 2FE3CB00.PicsArt-PhotoStudio | Remove-AppxPackage ;
-Get-AppxPackage 46928bounde.EclipseManager | Remove-AppxPackage ;
-Get-AppxPackage 4DF9E0F8.Netflix | Remove-AppxPackage ;
-Get-AppxPackage 613EBCEA.PolarrPhotoEditorAcademicEdition | Remove-AppxPackage ;
-Get-AppxPackage 6Wunderkinder.Wunderlist | Remove-AppxPackage ;
-Get-AppxPackage 7EE7776C.LinkedInforWindows | Remove-AppxPackage ;
-Get-AppxPackage 89006A2E.AutodeskSketchBook | Remove-AppxPackage ;
-Get-AppxPackage 9E2F88E3.Twitter | Remove-AppxPackage ;
-Get-AppxPackage A278AB0D.DisneyMagicKingdoms | Remove-AppxPackage ;
-Get-AppxPackage A278AB0D.MarchofEmpires | Remove-AppxPackage ;
-Get-AppxPackage ActiproSoftwareLLC.562882FEEB491 | Remove-AppxPackage ;
-Get-AppxPackage CAF9E577.Plex | Remove-AppxPackage ;
-Get-AppxPackage ClearChannelRadioDigital.iHeartRadio | Remove-AppxPackage ;
-Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage;
-Get-AppxPackage D52A8D61.FarmVille2CountryEscape | Remove-AppxPackage ;
-Get-AppxPackage D5EA27B7.Duolingo-LearnLanguagesforFree | Remove-AppxPackage ;
-Get-AppxPackage DB6EA5DB.CyberLinkMediaSuiteEssentials | Remove-AppxPackage ;
-Get-AppxPackage DolbyLaboratories.DolbyAccess | Remove-AppxPackage ;
-Get-AppxPackage Drawboard.DrawboardPDF | Remove-AppxPackage ;
-Get-AppxPackage Fitbit.FitbitCoach | Remove-AppxPackage ;
-Get-AppxPackage Flipboard.Flipboard | Remove-AppxPackage ;
-Get-AppxPackage GAMELOFTSA.Asphalt8Airborne | Remove-AppxPackage ;
-Get-AppxPackage KeeperSecurityInc.Keeper | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.549981C3F5F10 | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Appconnector | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingFinance | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingFoodAndDrink | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingHealthAndFitness | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingSports | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingTranslator | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingTravel | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.CommsPhone | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.ConnectivityStore | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Copilot | Remove-AppxPackage;
-Get-AppxPackage Microsoft.GamingServices | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.MicrosoftPowerBIForWindows | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.MicrosoftStickyNotes | Remove-AppxPackage;
-Get-AppxPackage Microsoft.MinecraftUWP | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.NetworkSpeedTest | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.People | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Print3D | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.ScreenSketch | Remove-AppPackage;
-Get-AppxPackage Microsoft.ScreenSketch | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Wallet | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage;
-Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.WindowsReadingList | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage;
-Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage ;
-Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage ;
-Get-AppxPackage MicrosoftCorporationII.MicrosoftFamily | Remove-AppxPackage;
-Get-AppxPackage MicrosoftTeams | Remove-AppxPackage;
-Get-AppxPackage MicrosoftWindows.CrossDevice | Remove-AppxPackage;
-Get-AppxPackage NORDCURRENT.COOKINGFEVER | Remove-AppxPackage ;
-Get-AppxPackage PandoraMediaInc.29680B314EFC2 | Remove-AppxPackage ;
-Get-AppxPackage Playtika.CaesarsSlotsFreeCasino | Remove-AppxPackage ;
-Get-AppxPackage ShazamEntertainmentLtd.Shazam | Remove-AppxPackage ;
-Get-AppxPackage SlingTVLLC.SlingTV | Remove-AppxPackage ;
-Get-AppxPackage ThumbmunkeysLtd.PhototasticCollage | Remove-AppxPackage ;
-Get-AppxPackage TuneIn.TuneInRadio | Remove-AppxPackage ;
-Get-AppxPackage XINGAG.XING | Remove-AppxPackage ;
-Get-AppxPackage flaregamesGmbH.RoyalRevolt2 | Remove-AppxPackage ;
-Get-AppxPackage king.com.* | Remove-AppxPackage ;
-Get-AppxPackage king.com.BubbleWitch3Saga | Remove-AppxPackage ;
-Get-AppxPackage king.com.CandyCrushSaga | Remove-AppxPackage ;
-Get-AppxPackage king.com.CandyCrushSodaSaga | Remove-AppxPackage ;
-Get-AppxPackage microsoft.windowscommunicationsapps | Remove-AppxPackage ;
+$packages = @(
+    "*3dbuilder*",
+    "*actiprosoftwarellc*",
+    "*adobephotoshopexpress*",
+    "*advertising.xaml*",
+    "*appconnector*",
+    "*asphalt8*",
+    "*autodesksketchbook*",
+    "*bethesda*",
+    "*bingfinance*",
+    "*bingfoodanddrink*",
+    "*binghealthandfitness*",
+    "*bingnews*",
+    "*bingsearch*",
+    "*bingsports*",
+    "*bingtranslator*",
+    "*bingtravel*",
+    "*bingweather*",
+    "*bubblewitch*",
+    "*caesarsslots*",
+    "*candycrush*",
+    "*clipchamp*",
+    "*connectivitystore*",
+    "*cookingfever*",
+    "*copilot*",
+    "*crossdevice*",
+    "*cyberlinkmediastudio*",
+    "*disney*",
+    "*dolby*",
+    "*drawboardpdf*",
+    "*duolingo*",
+    "*eclipse*",
+    "*facebook*",
+    "*farmville*",
+    "*feedbackhub*",
+    "*fitbit*",
+    "*flipboard*",
+    "*gamingservices*",
+    "*gethelp*",
+    "*getstarted*",
+    "*hiddencity*",
+    "*iheartradio*",
+    "*keeper*",
+    "*king.com*",
+    "*linkedin*",
+    "*marchofempires*",
+    "*messaging*",
+    "*microsoft.gamingapp*",
+    "*microsoft.powerautomatedesktop*",
+    "*microsoft.startexperiencesapp*",
+    "*microsoft.windows.devhome*",
+    "*microsoft3dviewer*",
+    "*microsoftfamily*",
+    "*minecraft*",
+    "*mixedreality*",
+    "*netflix*",
+    "*networkspeedtest*",
+    "*officehub*",
+    "*oneconnect*",
+    "*onenote*",
+    "*pandora*",
+    "*people*",
+    "*phone*",
+    "*phototastic*",
+    "*picsart*",
+    "*plex*",
+    "*polarr*",
+    "*powerbi*",
+    "*print3d*",
+    "*quickassist*",
+    "*readinglist*",
+    "*royalrevolt*",
+    "*screensketch*",
+    "*shazam*",
+    "*skype*",
+    "*sling*",
+    "*solitaire*",
+    "*soundrecorder*",
+    "*stickynotes*",
+    "*sway*",
+    "*todos*",
+    "*tunein*",
+    "*twitter*",
+    "*wallet*",
+    "*windowsalarms*",
+    "*windowscommunicationsapps*",
+    "*windowsfeedbackhub*",
+    "*windowsmaps*",
+    "*wunderlist*",
+    "*xbox*",
+    "*xing*",
+    "*zune*"
+)
 
-# Uninstall Windows Media Player
-Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue | Out-Null
+foreach ($package in $packages) {
+    Get-AppxPackage -Name $package | Remove-AppxPackage -ErrorAction SilentlyContinue
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like $package | Remove-AppxProvisionedPackage -Online
+}
+
 Write-Host -ForegroundColor Green "Done removing bloatware"
-
-# Uninstall Copilot
-Get-AppxPackage -AllUsers -Name "Microsoft.Windows.Copilot" | Remove-AppxPackage -AllUsers
-Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like "*Microsoft.Copilot*" } | Remove-AppxProvisionedPackage -Online
-
 exit 0
