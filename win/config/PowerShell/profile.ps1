9a0e74794fa83c5c4d9eb1693d1672a24a4d2a94
Akshay Brizmohun <aksh1000@gmail.com>
2026-06-01 18:02:17 +1000

    win: Add yazi alias to PowerShell profile

--- win/config/PowerShell/profile.ps1
+++ win/config/PowerShell/profile.ps1
@@ -36,6 +36,17 @@ function fzf-vim() {
     vim $(fzf)
 }
 
+# Yazi (https://yazi-rs.github.io/docs/quick-start)
+function y {
+	$tmp = (New-TemporaryFile).FullName
+	yazi.exe @args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if ($cwd -and $cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+	}
+	Remove-Item -Path $tmp
+}
+
 ################################################################################
 # Import Modules
 ################################################################################
